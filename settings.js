93fcba609ee5df4c18facb4360e835037cb6fb7a
liamking0046 <76438483+liamking0046@users.noreply.github.com>
2022-06-05 09:01:43 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'iam48', //0845a4544c9e\\
 }
 
 //customize settings\\
